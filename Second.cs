ad63857a45de0dabccf0a713c6b69b70332f9da3
Dabhi-Dhirajkumar <dhirajjdabhi1@gmail.com>
2025-07-09 23:13:05 +0530

    Add project files.

---
+++ Second.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        for (int i = 1; i >= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(); // Move to next line after each row
+        }
+    }
+}
---
+++ fourth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5; // Total number of rows
+
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(); // Move to next line after each row
+        }
+    }
+}
